9ed6faf4a7b52b08ac31cf41fce58449195b1a7a
0ambar <abrahamr677@gmail.com>
2024-12-03 15:18:04 -0600

    configruacion de estilos css con tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // Archivos donde encontrara las clases de css
+  content: ['./views/**/*.pug'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
